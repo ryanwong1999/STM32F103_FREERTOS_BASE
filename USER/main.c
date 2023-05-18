fabbd4ecebfc9b425aab08323493b5ec744fd31c
Ryan Wong <wshenyou@163.com>
2023-05-18 20:51:20 +0800

    first push

---
+++ USER/main.c
@@ -0,0 +1,23 @@
+#include "includes.h" 
+
+
+int main(void)
+{
+
+	 
+	taskENTER_CRITICAL();           //进入临界区
+		
+	//创建开始任务
+	xTaskCreate((TaskFunction_t )Start_Task,            //任务函数
+							(const char*    )"Start_Task",          //任务名称
+							(uint16_t       )START_STK_SIZE,        //任务堆栈大小
+							(void*          )NULL,                  //传递给任务函数的参数
+							(UBaseType_t    )START_TASK_PRIO,       //任务优先级
+							(TaskHandle_t*  )&Start_Task_Handler);   //任务句柄  
+
+	taskEXIT_CRITICAL();            //退出临界区	
+  vTaskStartScheduler();          //开启任务调度			
+	while(1);
+}
+
+
